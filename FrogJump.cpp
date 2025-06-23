454a315fa4d4aef7bfeac2726b88d7ca7b7e40b5
Anishk Chhamalwan <81318464+anishkchhamalwan@users.noreply.github.com>
2025-06-23 16:09:45 +0530

    Update FrogJump.cpp

--- FrogJump.cpp
+++ FrogJump.cpp
@@ -15,3 +15,18 @@ class Solution {
     }
 };
 
+int minCost(vector<int>& height) {
+        int n = height.size();
+        vector<int> dp(n,-1);
+        dp[0] =0;
+        for(int i=1;i<n;i++){
+            int oneJump = dp[i-1]+abs(height[i]-height[i-1]);
+            int twoJump = INT_MAX;
+            if(i>1) twoJump = dp[i-2]+abs(height[i]- height[i-2]);
+            dp[i] = min(oneJump,twoJump);
+        }
+        
+        
+        return dp[n-1];
+    }
+
